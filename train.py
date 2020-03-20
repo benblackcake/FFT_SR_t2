ec62131f3f31933ecb232d72bc65e081ef738a5b
ben <pingheng.chang@gmail.com>
2020-03-20 10:11:09 +0800

    test

--- train.py
+++ train.py
@@ -9,7 +9,7 @@ from matplotlib import pyplot as plt
 if __name__ == '__main__':
     img = 'images_train/butterfly.bmp'
     img = cv2.imread(img)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     print('img_shape ->',img.shape) #Y cr cb
 
@@ -23,7 +23,9 @@ if __name__ == '__main__':
         fftsr = FFTSR(sess, 1e-4, 15000)
 
         # fftsr.build_model()
-        fftsr.run(hr_img[:,:,0],lr_img[:,:,0])
+        # fftsr.run(hr_img[:,:,0],lr_img[:,:,0])
+        fftsr.run(hr_img,lr_img)
+
 
         # out = fftsr.pred
         # print(out)
