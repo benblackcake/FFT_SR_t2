7003f206bf9041df0f59f68f82c516fbac27bead
ben <pingheng.chang@gmail.com>
2020-03-21 01:18:53 +0800

    risidual test

--- model.py
+++ model.py
@@ -28,7 +28,7 @@ class FFTSR:
         self.label_risidual = self.label - self.images
         self.pred = tf.squeeze(self.model())
         self.pred = tf.real(tf.ifft2d(self.pred))
-        self.pred_risidual = self.label -self.label_risidual
+        self.pred_risidual = self.label -self.pred
         # self.label_risidual_fft = tf.complex(self.label_risidual, 0.0 * self.label_risidual) #self.label - self.images
 
         # self.pred = tf.squeeze(self.model())
@@ -43,7 +43,7 @@ class FFTSR:
         # print('pred',self.pred)
 
         # loss_complex = self.label_risidual - self.pred
-        self.loss = tf.nn.l2_loss(self.pred_risidual)
+        self.loss = tf.nn.l2_loss(self.label_risidual - self.pred_risidual)
         # squared_deltas = tf.square(self.label - self.pred)
         # self.loss = L2_loss(self.label, self.pred)
         # print(self.pred)
