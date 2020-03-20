c6b38769c2d8ee183d3c69cdc595aaeccf5445f5
ben <pingheng.chang@gmail.com>
2020-03-20 23:54:55 +0800

    risidual test

--- model.py
+++ model.py
@@ -237,7 +237,7 @@ class FFTSR:
         # #
         result = self.pred_risidual.eval({self.images: lr_img})
         # result = np.squeeze(result)
-        # result = result*255/(1e3*1e-5)
+        result = result*255/(1e3*1e-5)
         # result = np.clip(result, 0.0, 255.0).astype(np.uint8)
         plt_imshow((np.abs(result)))
         print(result)
