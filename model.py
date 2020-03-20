94800811503ce722fa57e314c412708ea0b0d665
ben <pingheng.chang@gmail.com>
2020-03-20 10:53:31 +0800

    test

--- model.py
+++ model.py
@@ -56,7 +56,7 @@ class FFTSR:
         print('f_',f_)
         print('__debug__spatial_c1',self.spectral_c1)
 
-        return f_
+        return f_ * self.spectral_c1
     #
 
     def fft_conv_pure(self, source, filters, width, height, stride, activation='relu', name='fft_conv'):
