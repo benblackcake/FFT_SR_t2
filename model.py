bb868fa058c386f563c5bbe19592a74d1abaf55e
ben <pingheng.chang@gmail.com>
2020-03-20 21:07:50 +0800

    test

--- model.py
+++ model.py
@@ -47,8 +47,8 @@ class FFTSR:
         self.f2,self.spectral_c2 = self.fft_conv_pure(self.f1,filters=5,width=256,height=256,stride=1, name='conv2')
         self.f3,self.spectral_c3 = self.fft_conv_pure(self.f2,filters=5,width=256,height=256,stride=1, name='conv3')
         self.f4,self.spectral_c4 = self.fft_conv_pure(self.f3,filters=5,width=256,height=256,stride=1, name='conv4')
-        self.f5,self.spectral_c5 = self.fft_conv_pure(self.f3,filters=5,width=256,height=256,stride=1, name='conv5')
-        self.f6,self.spectral_c6 = self.fft_conv_pure(self.f3,filters=5,width=256,height=256,stride=1, name='conv6')
+        self.f5,self.spectral_c5 = self.fft_conv_pure(self.f4,filters=5,width=256,height=256,stride=1, name='conv5')
+        self.f6,self.spectral_c6 = self.fft_conv_pure(self.f5,filters=5,width=256,height=256,stride=1, name='conv6')
 
         # self.f2,self.spectral_c2 = self.fft_conv_pure(self.f1,filters=5,width=256,height=256,stride=1, name='conv2')
         # self.f2,self.spectral_c2 = self.fft_conv_pure(self.f1,filters=5,width=256,height=256,stride=1, name='conv2')
